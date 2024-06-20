9033d112ce3ed4528c1ca9e7830f939a2e5d72c7
Haroun Rayes <bouzitharoun@gmail.com>
2024-06-21 00:16:38 +0200

    nothing

---
+++ main.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:isolate';
+
+// This function will be run in a separate isolate.
+void computeTask(SendPort sendPort) {
+  // Perform some computation
+  int result = 0;
+  for (int i = 0; i < 1000000; i++) {
+    result += i;
+  }
+
+  // Send the result back to the main isolate
+  sendPort.send(result);
+}
+
+Future<void> main() async {
+  // ReceivePort is used to receive messages from the isolate
+  final receivePort = ReceivePort();
+
+  // Spawn an isolate and run the computeTask function
+  await Isolate.spawn(computeTask, receivePort.sendPort);
+
+  // Wait for the result from the isolate
+  final result = await receivePort.first;
+  print('Result from isolate: $result');
+
+  receivePort.close();
+}
