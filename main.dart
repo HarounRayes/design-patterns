3307dfdd8d0139a588c20284a7cae31468efce8a
Haroun Rayes <bouzitharoun@gmail.com>
2024-08-20 18:59:49 +0200

    finish assis page with sone changes

--- main.dart
+++ main.dart
@@ -25,5 +25,8 @@ Future<void> main() async {
   print('Result from isolate: $result');
  
   // Close the receive port
+  // Close the receive port
+  // Close the receive port
+  // Close the receive port
   receivePort.close();
 }
